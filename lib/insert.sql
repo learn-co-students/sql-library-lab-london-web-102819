3387be4803c1f693e90412b5397a4e0f1f83a7ed
github username <github email address>
2019-11-06 14:45:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO Series (title, author_id, subgenre_id) VALUES ('sd',2,3);
+INSERT INTO Series (title, author_id, subgenre_id) VALUES ('sd',2,3);
+
+INSERT INTO Books (title, year, series_id) VALUES ('sdf',1232,2);
+INSERT INTO Books (title, year, series_id) VALUES ('sdf',1232,2);
+INSERT INTO Books (title, year, series_id) VALUES ('sdf',1232,2);
+INSERT INTO Books (title, year, series_id) VALUES ('sdf',1232,2);
+INSERT INTO Books (title, year, series_id) VALUES ('sdf',1232,2);
+INSERT INTO Books (title, year, series_id) VALUES ('sdf',1232,2);
+
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+INSERT INTO Characters (name, species, motto, author_id) VALUES ('sdf', 'sdf', 'sdf', 2);
+
+INSERT INTO Subgenres (name) VALUES ('sdf');
+INSERT INTO Subgenres (name) VALUES ('sdf');
+
+INSERT INTO Authors (name) VALUES ('sdf');
+INSERT INTO Authors (name) VALUES ('sdf');
+
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
